3843cace0317790b1b6fa2e05f0c06af2913ea59
bryanavalossistemas <bryanavalossistemas@gmail.com>
2025-08-28 18:46:30 -0500

    second commit

--- src/config/cors.ts
+++ src/config/cors.ts
@@ -2,7 +2,7 @@ import { CorsOptions } from "cors";
 
 export const corsConfig: CorsOptions = {
   origin: function (origin, callback) {
-    const whitelist = [process.env.FRONTEND_URL];
+    const whitelist = [process.env.FRONTEND_URL, "https://uptask-hujh.onrender.com"];
 
     if (process.argv[2] === "--api") {
       whitelist.push(undefined);
